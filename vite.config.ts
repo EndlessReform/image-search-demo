45c06ade744987b68b17b4f68fad4086e88539bd
Jacob Keisling <contact@keisling.me>
2024-08-20 23:20:25 -0500

    Add webgpu, silence fp32 warnings

--- vite.config.ts
+++ vite.config.ts
@@ -3,54 +3,62 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import Pages from "vite-plugin-pages";
 import getRepoName from "git-repo-name";
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ command, mode }) => {
-    const isGitHubPages = mode === 'github-pages'
-    const base = isGitHubPages ? `/${getRepoName.sync()}/` : '/'
-  
-    return {
-      plugins: [react(), Pages(), VitePWA({ 
-        registerType: 'autoUpdate', 
-        devOptions: { enabled: true},
-        includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
+  const isGitHubPages = mode === "github-pages";
+  const base = isGitHubPages ? `/${getRepoName.sync()}/` : "/";
+
+  return {
+    plugins: [
+      react(),
+      Pages(),
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: { enabled: true },
+        includeAssets: [
+          "favicon.svg",
+          "apple-touch-icon.png",
+          "masked-icon.svg",
+        ],
         manifest: {
-          name: 'My Image Search App',
-          short_name: 'ImgSearch',
-          description: 'Image Search Application',
-          theme_color: '#ffffff',
+          name: "ImageFinder",
+          short_name: "ImgSearch",
+          description: "Image Search Application",
+          theme_color: "#ffffff",
           icons: [
             {
-              src: 'pwa-192x192.png',
-              sizes: '192x192',
-              type: 'image/png'
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
             },
             {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png'
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
             },
             {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable'
-            }
-          ]
-        }
-      })],
-      base: base,
-      build: {
-        target: "esnext",
-        outDir: isGitHubPages ? 'dist-github' : 'dist'
-      },
-      resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "./src"),
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+          ],
         },
+      }),
+    ],
+    base: base,
+    build: {
+      target: "esnext",
+      outDir: isGitHubPages ? "dist-github" : "dist",
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-      optimizeDeps: {
-        exclude: ['@electric-sql/pglite'],
-      },
-    }
-  })
+    },
+    optimizeDeps: {
+      exclude: ["@electric-sql/pglite"],
+    },
+  };
+});
